8ea234c31d4fe700e175cf49e067342ca32a851d
notafishaxolotl <116303535+notafishaxolotl@users.noreply.github.com>
2025-11-25 09:49:21 +0000

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+//#include <string>
+//#include <ctime>
+//#include<list>
+//#include <algorithm>
+
+//using std::cout;
+//using str = std::string;
+//using std::cin;
+//using std::endl;
+//using std::tolower;
+//using std::toupper;
+/*using std::getline;
+using std::ws;*/
+//using std::list;
+//using std::find;
+
+void x();//replace x for new function name
+int main() {
+    return 0;
+}
+void x(){ //this can repllace line14
+}
+
+//lines 6-14 can be replaced with "using namespace std;" 
